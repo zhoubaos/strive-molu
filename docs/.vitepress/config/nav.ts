406a38802d8e5c9f4004c0004f5df298ddc84aab
zhoubaos <15730508978@163.com>
2025-08-27 18:12:49 +0800

    docs(docs): :memo: 添加strive-molu-axios文档

--- docs/.vitepress/config/nav.ts
+++ docs/.vitepress/config/nav.ts
@@ -3,6 +3,6 @@ import type { NavOption } from '../typescript/option';
 export const nav: NavOption[] = [
   { text: '组件', link: '/pages/component/table', activeMatch: '/component/' },
   { text: '指令', link: '/pages/directive/number-lazy', activeMatch: '/directive/' },
-  { text: '工具函数', link: '/pages/util/tree-tools', activeMatch: '/util/' }
-  // {text:'API请求',link:'/pages/api/request',activeMatch:'/api/'}
+  { text: '工具函数', link: '/pages/util/tree-tools', activeMatch: '/util/' },
+  { text: 'Fetch工具', link: '/pages/strive-molu-axios', activeMatch: '/strive-molu-axios' }
 ];
